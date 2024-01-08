af5547a1665880493c05590c28dc024983b0a98b
Rune Højsgaard <rho@reviso.com>
2024-01-08 23:24:39 +0100

    Separated slack code

---
+++ src/Aula/Html2SlackMarkdownConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Html2Markdown;
+
+namespace Aula
+{
+ public class Html2SlackMarkdownConverter
+ {
+	 private readonly Converter _converter;
+
+	 public Html2SlackMarkdownConverter()
+	 {
+		 _converter = new Converter();
+	 }
+	 public string Convert(string? html)
+	 {
+		 return _converter.Convert(html ?? "").Replace("**", "*");
+  }
+ }
+}
