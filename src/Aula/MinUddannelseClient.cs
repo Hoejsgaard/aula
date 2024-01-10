7949f081205244ea1d230c706fcc243be3c2912b
Rune Højsgaard <rho@reviso.com>
2024-01-10 10:30:29 +0100

    Moved UniLogin flow code to separate class. Nasty new method, that should probably be mitigate with a delegate, but will work for now

--- src/Aula/MinUddannelseClient.cs
+++ src/Aula/MinUddannelseClient.cs
@@ -1,31 +1,18 @@
 ﻿using System.Globalization;
-using System.Net;
 using System.Net.Http.Headers;
-using System.Web;
 using HtmlAgilityPack;
 using Newtonsoft.Json.Linq;
 
 namespace Aula;
 
-public class MinUddannelseClient
+public class MinUddannelseClient : UniLoginClient
 {
-	private readonly HttpClient _httpClient;
-	private readonly string _password;
-	private readonly string _username;
-	private bool _loggedIn;
 	private JObject _userProfile = new();
 
-	public MinUddannelseClient(string username, string password)
+	public MinUddannelseClient(string username, string password) : base(username, password,
+		"https://www.minuddannelse.net/KmdIdentity/Login?domainHint=unilogin-idp-prod&toFa=False",
+		"https://www.minuddannelse.net/Node/")
 	{
-		var httpClientHandler = new HttpClientHandler
-		{
-			CookieContainer = new CookieContainer(),
-			UseCookies = true,
-			AllowAutoRedirect = true
-		};
-		_httpClient = new HttpClient(httpClientHandler);
-		_username = username ?? throw new ArgumentNullException(nameof(username));
-		_password = password ?? throw new ArgumentNullException(nameof(password));
 	}
 
 	public async Task<JObject> GetWeekLetter(Child? find)
@@ -33,7 +20,7 @@ public class MinUddannelseClient
 		// hardcoded URL just to test that it works
 		var url = "https://www.minuddannelse.net/api/stamdata/ugeplan/getUgeBreve?tidspunkt=2024-W2&elevId=2643430&_=" +
 		          DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-		var response = await _httpClient.GetAsync(url);
+		var response = await HttpClient.GetAsync(url);
 		response.EnsureSuccessStatusCode();
 		var json = await response.Content.ReadAsStringAsync();
 		return JObject.Parse(json);
@@ -41,9 +28,10 @@ public class MinUddannelseClient
 
 	public async Task<JObject> GetWeekLetter(Child child, DateOnly date)
 	{
-		var url = string.Format("https://www.minuddannelse.net/api/stamdata/ugeplan/getUgeBreve?tidspunkt={0}-W{1}&elevId={2}&_={3}"
+		var url = string.Format(
+			"https://www.minuddannelse.net/api/stamdata/ugeplan/getUgeBreve?tidspunkt={0}-W{1}&elevId={2}&_={3}"
 			, date.Year, GetIsoWeekNumber(date), GetChildId(child), DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-		var response = await _httpClient.GetAsync(url);
+		var response = await HttpClient.GetAsync(url);
 		response.EnsureSuccessStatusCode();
 		var json = await response.Content.ReadAsStringAsync();
 		return JObject.Parse(json);
@@ -55,19 +43,14 @@ public class MinUddannelseClient
 		//		var content = weekLetter["ugebreve"]?[0]?["indhold"]?.ToString() ?? "";
 		var kids = _userProfile["boern"];
 		if (kids == null) throw new Exception("No children found in user profile");
-		string id = "";
+		var id = "";
 		foreach (var kid in kids)
-		{
 			if (kid["fornavn"]?.ToString() == child.FirstName)
-			{
 				id = kid["id"]?.ToString() ?? "";
-			}
-		}
 
 		if (id == "") throw new Exception("Child not found");
-		
-		return id;
 
+		return id;
 	}
 
 	private int GetIsoWeekNumber(DateOnly date)
@@ -78,56 +61,33 @@ public class MinUddannelseClient
 		return cultureInfo.Calendar.GetWeekOfYear(date.ToDateTime(TimeOnly.MinValue), calendarWeekRule, firstDayOfWeek);
 	}
 
-	public async Task<bool> LoginAsync()
+	public new async Task<bool> LoginAsync()
 	{
-		var loginUrl = "https://www.minuddannelse.net/KmdIdentity/Login?domainHint=unilogin-idp-prod&toFa=False";
-		var response = await _httpClient.GetAsync(loginUrl);
-		var content = await response.Content.ReadAsStringAsync();
+		var login = await base.LoginAsync();
+		if (login)
+		{
+			HttpClient.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+			_userProfile = await ExtractUserProfile();
 
-		return await ProcessLoginResponseAsync(content);
-	}
+			//Console.WriteLine("MinUddannelse Profile");
+			//Console.WriteLine(Program.PrettifyJson(_userProfile.ToString()));
 
+			return true;
+		}
 
-	private async Task<bool> ProcessLoginResponseAsync(string content)
-	{
-		var maxSteps = 10;
-		var success = false;
-		for (var stepCounter = 0; stepCounter < maxSteps; stepCounter++)
-			try
-			{
-				var formData = ExtractFormData(content);
-				var response = await _httpClient.PostAsync(formData.Item1, new FormUrlEncodedContent(formData.Item2));
-				content = await response.Content.ReadAsStringAsync();
-
-
-				success = CheckIfLoginSuccessful(response);
-				if (success)
-				{
-					_httpClient.DefaultRequestHeaders.Accept.Add(
-						new MediaTypeWithQualityHeaderValue("application/json"));
-					_userProfile = ExtractUserProfile(content);
-
-					//Console.WriteLine("MinUddannelse Profile");
-					//Console.WriteLine(Program.PrettifyJson(_userProfile.ToString()));
-
-					return true;
-				}
-			}
-			catch (Exception)
-			{
-				// ignored - this is kind of fragile
-			}
-
-		return success;
+		return login;
 	}
 
 	/// <summary>
-	///  Read profile data out of the front page. I can't find the api call, if it exist, that give me this data
+	///     Read profile data out of the front page. I can't find the api call, if it exist, that give me this data
 	/// </summary>
-	private JObject ExtractUserProfile(string html)
+	private async Task<JObject> ExtractUserProfile()
 	{
+		var response = await HttpClient.GetAsync(SuccessUrl);
+		var content = await response.Content.ReadAsStringAsync();
 		var doc = new HtmlDocument();
-		doc.LoadHtml(html);
+		doc.LoadHtml(content);
 
 		// Find the script node that contains __tempcontext__
 		var script = doc.DocumentNode.Descendants("script")
@@ -144,59 +104,5 @@ public class MinUddannelseClient
 		var jsonText = scriptText.Substring(startIndex, endIndex - startIndex).Trim();
 
 		return JObject.Parse(jsonText);
-	}
-
-
-	private Tuple<string, Dictionary<string, string>> ExtractFormData(string htmlContent)
-	{
-		var doc = new HtmlDocument();
-		doc.LoadHtml(htmlContent);
-		var formNode = doc.DocumentNode.SelectSingleNode("//form");
-
-		if (formNode == null) throw new Exception("Form not found");
-
-		var actionUrl = formNode.Attributes["action"]?.Value;
-		if (actionUrl == null) throw new Exception("No action node found");
-		var formData = BuildFormData(doc);
-		var writer = new StringWriter();
-		HttpUtility.HtmlDecode(actionUrl, writer);
-		var decodedUrl = writer.ToString();
-		return new Tuple<string, Dictionary<string, string>>(decodedUrl, formData);
-	}
-
-	private Dictionary<string, string> BuildFormData(HtmlDocument document)
-	{
-		var formData = new Dictionary<string, string>();
-
-		var inputs = document.DocumentNode.SelectNodes("//input");
-		if (inputs == null)
-		{
-			formData.Add("selectedIdp", "uni_idp");
-			return formData;
-		}
-
-		foreach (var input in inputs)
-		{
-			var name = input.Attributes["name"]?.Value;
-			var value = input.GetAttributeValue("value", string.Empty);
-
-			if (string.IsNullOrWhiteSpace(name)) continue;
-
-			formData[name] = name switch
-			{
-				"username" => _username,
-				"password" => _password,
-				_ => value
-			};
-		}
-
-		return formData;
-	}
-
-	private bool CheckIfLoginSuccessful(HttpResponseMessage response)
-	{
-		_loggedIn = response.RequestMessage?.RequestUri?.ToString() == "https://www.minuddannelse.net/Node/";
-
-		return _loggedIn;
 	}
 }
--- src/Aula/Program.cs
+++ src/Aula/Program.cs
@@ -19,17 +19,17 @@ public class Program
 		
 		var slackBot = new SlackBot(config.Slack.WebhookUrl);
 
-		var minUddannelseClient =
-			new MinUddannelseClient(config.AulaCredentials.Username, config.AulaCredentials.Password);
-		if (await minUddannelseClient.LoginAsync())
-			Console.WriteLine("Login to MinUddannelse successful.");
-		else
-			Console.WriteLine("Login to MinUddannelse failed.");
+		//var minUddannelseClient =
+		//	new MinUddannelseClient(config.AulaCredentials.Username, config.AulaCredentials.Password);
+		//if (await minUddannelseClient.LoginAsync())
+		//	Console.WriteLine("Login to MinUddannelse successful.");
+		//else
+		//	Console.WriteLine("Login to MinUddannelse failed.");
 
-		Child child = config.Children[0];
+		//Child child = config.Children[0];
 
-		var weekLetter = await minUddannelseClient.GetWeekLetter(child, DateOnly.FromDateTime(DateTime.Today));
-		await slackBot.PushWeekLetterFancy(weekLetter, child);
+		//var weekLetter = await minUddannelseClient.GetWeekLetter(child, DateOnly.FromDateTime(DateTime.Today));
+		//await slackBot.PushWeekLetterFancy(weekLetter, child);
 
 		var aulaClient = new AulaClient(config.AulaCredentials.Username, config.AulaCredentials.Password);
 		if (await aulaClient.LoginAsync())
@@ -37,17 +37,17 @@ public class Program
 		else
 			Console.WriteLine("Login to Aula failed.");
 
-		//var profile = await aulaClient.GetProfile();
-		//var profileContext = await aulaClient.GetProfileContext();
+		var profile = await aulaClient.GetProfile();
+		var profileContext = await aulaClient.GetProfileContext();
 
 
-	//	Console.WriteLine("Profile: ");
-	//	Console.WriteLine(PrettifyJson(profile.ToString()));
+		Console.WriteLine("Profile: ");
+		Console.WriteLine(PrettifyJson(profile.ToString()));
 
-	//	Console.WriteLine();
-	//	Console.WriteLine("Profile Context: ");
-	//	Console.WriteLine(PrettifyJson(profileContext.ToString()));
-	//	Console.WriteLine();
+		Console.WriteLine();
+		Console.WriteLine("Profile Context: ");
+		Console.WriteLine(PrettifyJson(profileContext.ToString()));
+		Console.WriteLine();
 	}
 
 	public static string PrettifyJson(string json)
