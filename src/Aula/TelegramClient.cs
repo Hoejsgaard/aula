f4b0f777f05394c3d566e0c6286595ddd6e9c446
Rune Højsgaard <rho@reviso.com>
2024-01-15 00:05:02 +0100

    Cleaning up Telegram client

--- src/Aula/TelegramClient.cs
+++ src/Aula/TelegramClient.cs
@@ -1,55 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using Slack.Webhooks;
+﻿using Newtonsoft.Json.Linq;
 using Telegram.Bot;
-using ParseMode = Telegram.Bot.Types.Enums.ParseMode;
+using Telegram.Bot.Types.Enums;
 
-namespace Aula
+namespace Aula;
+
+public class TelegramClient
 {
-	public class TelegramClient
+	private readonly Html2SlackMarkdownConverter _markdownConverter;
+	private readonly ITelegramBotClient _telegram;
+
+	public TelegramClient(string token)
 	{
-		private readonly ITelegramBotClient _telegram;
-		private readonly Html2SlackMarkdownConverter _markdownConverter;
+		_telegram = new TelegramBotClient(token);
+		_markdownConverter = new Html2SlackMarkdownConverter();
+	}
 
-		public TelegramClient(string token)
+	public async Task<bool> SendMessageToChannel(string channelId, string message)
+	{
+		try
 		{
-			_telegram = new TelegramBotClient(token);
-			_markdownConverter = new Html2SlackMarkdownConverter();
+			await _telegram.SendTextMessageAsync(
+				channelId,
+				message,
+				parseMode: ParseMode.Markdown
+			);
+			return true;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error sending message: {ex.Message}");
 		}
 
-		//Group id: -4070356654
-		public async Task<bool> SendMessageToChannel(string channelId, string message)
-		{
-			try
-			{
-				await _telegram.SendTextMessageAsync(
-					chatId: channelId,
-					text: message,
-					parseMode: ParseMode.Markdown
-				);
-				return true;
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Error sending message: {ex.Message}");
-			}
-			return false;
-		}
+		return false;
+	}
 
-		public async Task<bool> PostWeekLetter(string channelId, JObject weekLetter)
-		{
-			var @class = weekLetter["ugebreve"]?[0]?["klasseNavn"]?.ToString() ?? "";
-			var week = weekLetter["ugebreve"]?[0]?["uge"]?.ToString() ?? "";
-			var letterText = _markdownConverter.Convert(weekLetter["ugebreve"]?[0]?["indhold"]?.ToString() ?? "").Replace("**", "*");
-			string message = string.Format(@$"*Ugebrev for {@class} uge {2}*
+	public async Task<bool> PostWeekLetter(string channelId, JObject weekLetter)
+	{
+		var @class = weekLetter["ugebreve"]?[0]?["klasseNavn"]?.ToString() ?? "";
+		var week = weekLetter["ugebreve"]?[0]?["uge"]?.ToString() ?? "";
+		var letterText = _markdownConverter.Convert(weekLetter["ugebreve"]?[0]?["indhold"]?.ToString() ?? "")
+			.Replace("**", "*");
+		var message = string.Format(@$"*Ugebrev for {@class} uge {2}*
 {letterText}
 			");
 
-			return await SendMessageToChannel(channelId, message);
-		}
+		return await SendMessageToChannel(channelId, message);
 	}
 }
